01b46d66ec83cedcc6def1196b6120d2635e31ff
gab0626 <kyhk.0626@outlook.jp>
2015-11-24 02:42:06 +0900

    エラー無し版
    修繕版

--- whiteblack/Block/Block.cpp
+++ whiteblack/Block/Block.cpp
@@ -3,7 +3,7 @@
 
 Block::Block(){
     setup();
-    
+	type = static_cast<int>(BLOCK::NULLBLOCK);
 }
 
 void Block::update(){
@@ -18,10 +18,52 @@ void Block::update(){
 
         break;
     }
+
+	if (env.isPushButton(Mouse::LEFT))
+	{
+		type = (type + 1) % 8;
+	}
     
 }
 
 void Block::draw(){
+
+	switch (type)
+	{
+	case static_cast<int>(BLOCK::NULLBLOCK) :
+		break;
+
+	case static_cast<int>(BLOCK::NORMAL_) :
+		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::white);
+		break;
+
+	case static_cast<int>(BLOCK::WHITE) :
+		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::black);
+		break;
+
+	case static_cast<int>(BLOCK::BLACK_) :
+		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::blue);
+		break;
+
+	case static_cast<int>(BLOCK::MOVE_) :
+		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::cyan);
+		break;
+
+	case static_cast<int>(BLOCK::FALL_) :
+		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::gray);
+		break;
+
+	case static_cast<int>(BLOCK::DOUBLE_W) :
+		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::green);
+		break;
+
+	case static_cast<int>(BLOCK::DOUBLE_B) :
+		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::lime);
+		break;
+	}
+
+
+
 
 }
 
