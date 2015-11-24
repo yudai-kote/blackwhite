06b2852d952bb426f5095cbed52c5601bf63d7a8
ryohalon <soccer.ry05@gmail.com>
2015-11-24 18:54:56 +0900

    map
    エラー消し

--- whiteblack/Block/Block.cpp
+++ whiteblack/Block/Block.cpp
@@ -3,68 +3,25 @@
 
 Block::Block(){
     setup();
-	type = static_cast<int>(BLOCK::NULLBLOCK);
+    
 }
 
 void Block::update(){
-    
-    switch (static_cast<int>(status))
+    switch (status)
     {
-    case static_cast<int>(BLOCK::BLACK_) :
+    case BLOCK::FALL_:
         break;
-    case static_cast<int>(BLOCK::FALL_BLACK_) :
+    case BLOCK::MOVE_:
         break;
     default:
 
 
         break;
     }
-
-	if (env.isPushButton(Mouse::LEFT))
-	{
-		type = (type + 1) % 8;
-	}
     
 }
 
 void Block::draw(){
-
-	switch (type)
-	{
-	case static_cast<int>(BLOCK::NULLBLOCK) :
-		break;
-
-	case static_cast<int>(BLOCK::NORMAL_) :
-		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::white);
-		break;
-
-	case static_cast<int>(BLOCK::WHITE) :
-		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::black);
-		break;
-
-	case static_cast<int>(BLOCK::BLACK_) :
-		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::blue);
-		break;
-
-	case static_cast<int>(BLOCK::MOVE_) :
-		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::cyan);
-		break;
-
-	case static_cast<int>(BLOCK::FALL_) :
-		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::gray);
-		break;
-
-	case static_cast<int>(BLOCK::DOUBLE_W) :
-		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::green);
-		break;
-
-	case static_cast<int>(BLOCK::DOUBLE_B) :
-		drawFillBox(start_pos.x(), start_pos.y(), draw_pos.x(), draw_pos.y(), Color::lime);
-		break;
-	}
-
-
-
 
 }
 
@@ -95,11 +52,11 @@ Vec2f Block::Collision(Object _player,
 
 
 
-    switch (static_cast<int>(status))
+    switch (status)
     {
-    case static_cast<int>(BLOCK::BLACK_):
+    case BLOCK::FALL_:
         break;
-    case static_cast<int>(BLOCK::FALL_BLACK_):
+    case BLOCK::MOVE_:
         break;
     default:
         break;
