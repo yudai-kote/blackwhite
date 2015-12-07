2f02214008379b62cd6ac9689a0432197c6edeb1
R.komatsu <rkc_tw_st@yahoo.co.jp>
2015-12-07 22:54:02 +0900

    更新

--- whiteblack/Block/BlockBase.h
+++ whiteblack/Block/BlockBase.h
@@ -9,11 +9,6 @@ public:
 	virtual void draw() = 0;
 	virtual void setup() = 0;
 
-	virtual Vec2f Collision(Object _player,
-		BLOCK _up, BLOCK _down,
-		DIRECTION _direction = DIRECTION::NON,
-		int move_vector = 0) = 0;
-
 	void setPos(Vec2f _pos){ block.pos = _pos; }
 	void setStatus(BLOCK _block){ status = _block; }
 	void setCondition(CONDITION _type){ color_type = _type; }
