04b777fe98067efdf156b8054452be0925391056
PTxDE*SS <desspertsouji@yahoo.co.jp>
2015-12-12 16:13:12 +0900

    重力上限追加

--- whiteblack/Player/Player.cpp
+++ whiteblack/Player/Player.cpp
@@ -11,7 +11,20 @@ void Player::update(){
 }
 
 void Player::draw(){
-	
+	drawTextureBox(
+		player.pos.x(),
+		player.pos.y(),
+		player.size.x(),
+		player.size.y(),
+		0 + cut_x,
+		0,
+		256,
+		256,
+		player_texture,
+		Color::white,
+		0, Vec2f(-1, 1),
+		Vec2f(player.size.x() / 2, 0)
+		);
 	if (direction == DIRECTION::LEFT){
 
 		drawTextureBox(
@@ -141,7 +154,7 @@ void Player::move(){
 		player.vec.y() = speed.y();
 	}
 	player.pos.y() += player.vec.y();
-	if (g <= 20){
+	if (player.vec.y() >= -50){
 		player.vec.y() -= g;
 	}
 }
